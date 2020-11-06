f9f4a1ab1615ea394dc448537a240c1261500791
nevinhall <46722804+nevinhall@users.noreply.github.com>
2020-11-06 11:12:24 +0000

    begin menu implementation

--- src/main/java/org/springProject/Main.java
+++ src/main/java/org/springProject/Main.java
@@ -4,10 +4,104 @@ import org.springProject.service.PersonService;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
         PersonService personService = (PersonService) context.getBean("personServiceImplementation");
+
+        Boolean runApp = true;
+        int runDefaults = 1;
+        while (runApp) {
+
+            if (runDefaults == 1) {
+                System.out.println("A run defaults \n");
+            }
+            System.out.println("Enter Choice:");
+            System.out.println("1. Delete a Person");
+            System.out.println("2. Average Age of House Occupants");
+            System.out.println("3. Number of Old Age Pensioners in DB");
+            System.out.println("4. Number of Students in DB");
+            System.out.println("5. Search Household and list given Occupants");
+            System.out.println("6. Move person From one Household to another");
+            System.out.println("6. Move person From one Household to another");
+            System.out.println("7. Add a household, along with its occupant(s)");
+            System.out.println("8. Add a new person and assign that person to a household");
+
+
+            Scanner sc = new Scanner(System.in);
+            String userInput = sc.nextLine();
+            System.out.println(userInput);
+
+            switch (userInput) {
+                case "1":
+
+                    System.out.println("You Have Entered to Delete a Person");
+                    System.out.println(personService.deleteHero(1));
+                    runDefaults = 0;
+
+                    break;
+                case "2":
+
+                    System.out.println("You Have Entered to View The Average Age of House Occupants");
+                    System.out.println(personService.avg_age());
+                    runDefaults = 0;
+
+                    break;
+                case "3":
+
+                    System.out.println("You Have Entered to view the number of Old Age Pensioners in DB");
+                    System.out.println(personService.countOAP());
+                    runDefaults = 0;
+
+                    break;
+                case "4":
+
+                    System.out.println("You Have Entered to view the Number of Students in DB");
+                    System.out.println(personService.countStudents());
+                    runDefaults = 0;
+
+                    break;
+                case "5":
+
+                    System.out.println("You Have Entered to Search a Household and list given Occupants");
+                    System.out.println(personService.searchHousehold("000"));
+                    runDefaults = 0;
+
+                    break;
+                case "6":
+
+                    System.out.println("You Have Entered to Move a person From one Household to another");
+                    System.out.println(personService.moveHouse(4, "p8k0"));
+                    runDefaults = 0;
+
+                    break;
+                case "7":
+                    System.out.println("You Have Entered to Add a household, along with its occupant(s)");
+                    runDefaults = 0;
+
+                    break;
+                case "8":
+                    System.out.println("You Have Entered to Add a new person and assign that person to a household ");
+                    runDefaults = 0;
+
+                    break;
+            }
+
+
+            if (userInput.equals("0")) {
+                runApp = false;
+                System.out.println("Application closing");
+            }
+
+            System.out.println("\n**************************************** \n");
+
+
+        }
+
+
+        System.out.println(personService.deleteHero(1));
 
 
         System.out.println(personService.deleteHero(1));
@@ -19,11 +113,9 @@ public class Main {
 
         System.out.println(personService.searchHousehold("000"));
 
+        System.out.println(personService.searchHousehold("0000"));
+
         System.out.println(personService.moveHouse(4, "p8k0"));
-
-
-
-
 
 
     }
